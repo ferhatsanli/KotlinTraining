a932804899ed62747995045f8f6ede670c241a08
Ferhat Sanli <ferhatsanli.nl@gmail.com>
2024-09-17 21:55:49 +0200

    Null topic is done.

--- src/arrays/nullchars.kt
+++ src/arrays/nullchars.kt
@@ -1,5 +1,20 @@
 package arrays
 
 fun main() {
+    val nullVar : Boolean? = null
+    val nullStr : String? = null
 
+    println(nullVar)
+    println(nullStr?.length ?: "Empty variable.")
+    // if you want to get an error then...
+    //println(nullStr!!.length)
+
+    // Int variable type can't store null
+    // so if you want to store null in it
+    // then you should make it "Int?"
+    var myNum : Int?
+    myNum = 3
+    println(myNum)
+    myNum = null
+    println("The length of a null variable is ${myNum.toString().length}")
 }
